60e034053066ce205242cc96b6b86d183204cdc3
Simon <simon.ebsworth@yahoo.co.uk>
2023-04-22 19:04:49 +0100

    Commented out 'for' block in main class

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        Match.play();
 
 
 
-        double count = 0;
-        for (int i = 0; i < 1; i++) {
-            Match.play();
-            count++;
-        }
-
-        double totalHomeGoals = Match.homeTeam.getGoalsScored();
-        double totalAwayGoals = Match.awayTeam.getGoalsScored();
-
-        double homeAvgGoals = totalHomeGoals / count;
-        double awayAvgGoals = totalAwayGoals / count;
-
-        System.out.println("Total home goals: " + totalHomeGoals);
-        System.out.println("Average home goals: " + homeAvgGoals);
-        System.out.println();
-        System.out.println("Total away goals: " + totalAwayGoals);
-        System.out.println("Average away goals " + awayAvgGoals);
+//        double count = 0;
+//        for (int i = 0; i < 1; i++) {
+//            Match.play();
+//            count++;
+//        }
+//
+//        double totalHomeGoals = Match.homeTeam.getGoalsScored();
+//        double totalAwayGoals = Match.awayTeam.getGoalsScored();
+//
+//        double homeAvgGoals = totalHomeGoals / count;
+//        double awayAvgGoals = totalAwayGoals / count;
+//
+//        System.out.println("Total home goals: " + totalHomeGoals);
+//        System.out.println("Average home goals: " + homeAvgGoals);
+//        System.out.println();
+//        System.out.println("Total away goals: " + totalAwayGoals);
+//        System.out.println("Average away goals " + awayAvgGoals);
     }
 }
