3f8d242e4dfc623a70b5775adf12e9e0e21b72fc
liamebs <simon.ebsworth@yahoo.co.uk>
2023-04-23 08:28:59 +0100

    First steps to implement player class

---
+++ src/Player.java
@@ -0,0 +1,25 @@
+public class Player {
+    private String firstName;
+    private String lastName;
+    private String position;
+    private String nationality;
+    private String preferredFoot;
+    private int height;
+    private int age;
+
+    // create constructor for Player class and initialise instance variables
+    public Player(String firstName, String lastName, String position,
+           String nationality, String preferredFoot, int height, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.nationality = nationality;
+        this.preferredFoot = preferredFoot;
+        this.height = height;
+        this.age = age;
+    }
+
+
+
+
+}
